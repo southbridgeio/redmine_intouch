0368296cf7fdb904daaf7764c895647c22718f7e
Vladislav Yashin <v.yashin.work@gmail.com>
2021-02-11 13:13:14 +0300

    Fix TelegramGroupMessageSender

--- app/models/telegram_group_chat.rb
+++ app/models/telegram_group_chat.rb
@@ -1,6 +1,6 @@
 class TelegramGroupChat < ActiveRecord::Base
   def self.find_by_tid(chat_id)
     # Telegram group id is always negative
-    where("ABS(#{table_name}.chat_id) = ?", chat_id.abs).take
+    where("ABS(#{table_name}.tid) = ?", chat_id.abs).take
   end
 end
